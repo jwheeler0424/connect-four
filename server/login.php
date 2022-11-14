0abccb1735418caf00033f599f96d521a89eefb0
jwheeler0424 <jonathan.wheeler87@gmail.com>
2022-11-13 17:43:11 -0800

    update login

--- server/login.php
+++ server/login.php
@@ -5,7 +5,7 @@
  * @author Jonathan Wheeler <jwheeler0424@mail.fresnostate.edu>
  */
 //include_once('./Database.php');
-date_default_timezone_set('America/Los_Angeles');
+//date_default_timezone_set('America/Los_Angeles');
 $date = new DateTime();
 
 // echo $_POST['username'];
